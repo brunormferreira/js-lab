be165117637c7631404a43d774778cf04b83031b
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-04 22:15:04 -0300

    add: refact 04 and exercise 05

---
+++ arrays/exercises/exercise-5.js
@@ -0,0 +1,6 @@
+// Exercício 5 - Sem duplicações
+// Faça uma função chamada removeDuplicatas que recebe um array de números inteiros e remove todas as suas duplicadas.
+
+// Utilize as funções auxiliares: reduce e find.
+
+// Exemplo: removeDuplicatas([1,2,3,3,4,5]) → [1,2,3,4,5]
