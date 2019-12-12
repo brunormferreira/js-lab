61f04438e57706c1d82a81098ba5bdb1f5daa1f0
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-12 16:11:40 -0300

    add: exercise 09 to do

---
+++ arrays/exercises/exercise-9.js
@@ -0,0 +1,5 @@
+// Exercício 11 - A pequena ovelha Dolly
+// Utilizando o método auxiliar forEach, crie uma função clonaObjeto que 
+// recebe como parâmetro um objeto e cria uma cópia exata dela.
+
+// utilize o método Object.getOwnPropertyNames para obter as propriedades do objeto
