e16de8b8702daf2e6320bf90522ffa2de9e62bff
Bruno Ramires <brunormferreira@hotmail.com>
2019-12-26 22:52:31 -0300

    add: side effects example

---
+++ functional-programming/side-effects.js
@@ -0,0 +1,19 @@
+let meetup = { name: 'JS', isActive: true, members: 49 };
+
+const scheduleMeetup = (date, place) => {
+  meetup.date = date;
+  meetup.place = place;
+  if (meetup.members < 50)
+    meetup.isActive = false;
+}
+const publishMeetup = () => {
+  if (meetup.isActive) {
+    meetup.publish = true;
+  }
+}
+
+scheduleMeetup('today','Bnagalore');
+publishMeetup();
+console.log(meetup);
+
+// Function or expression is said to have a side effect if it modifies some state of the program, which is outside of its own scope or has an observable interaction with its calling functions or the outside program besides returning a value.
