eb01979e04028a57304f22539c4dde701d8f180e
brunormferreira <brunormferreira@hotmail.com>
2020-01-02 22:47:37 -0300

    fix: text explanation

--- functional-programming/side-effects.js
+++ functional-programming/side-effects.js
@@ -16,4 +16,6 @@ scheduleMeetup('today','Bnagalore');
 publishMeetup();
 console.log(meetup);
 
-// Function or expression is said to have a side effect if it modifies some state of the program, which is outside of its own scope or has an observable interaction with its calling functions or the outside program besides returning a value.
+// Function or expression is said to have a side effect if it modifies some state of the program,
+// which is outside of its own scope or has an observable interaction with its calling functions or
+// the outside program besides returning a value.
