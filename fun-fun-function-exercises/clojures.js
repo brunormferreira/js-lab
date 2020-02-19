e9abce43a002ff3f5e137a04e6e89ee67197f385
Bruno Ramires <brunormferreira@hotmail.com>
2020-02-19 15:01:27 -0300

    add more one example with clojures

--- fun-fun-function-exercises/clojures.js
+++ fun-fun-function-exercises/clojures.js
@@ -14,4 +14,13 @@ function init() {
   displayName();    
 }
 init();
-  
+
+// function makeSizer(size) {
+//   return function() {
+//     document.body.style.fontSize = size + 'px';
+//   };
+// }
+
+// let size12 = makeSizer(12);
+// let size14 = makeSizer(14);
+// let size16 = makeSizer(16);
