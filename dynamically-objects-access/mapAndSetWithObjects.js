fd48b63a6b3ef2176494d3ed4e98a5135b254f2a
brunormferreira <brunormferreira@hotmail.com>
2020-03-05 21:03:20 -0300

    add map and sets with objects studies

---
+++ dynamically-objects-access/mapAndSetWithObjects.js
@@ -0,0 +1,78 @@
+const birds = {
+  owl: '🦉',
+  eagle: '🦅',
+  duck: '🦆'
+};
+
+const entries = Object.entries(birds);
+console.log(entries);
+
+// [['owl', '🦉'], ['eagle', '🦅'], ['duck', '🦆']]
+
+/* ----------------------------------------------------------------------------- */
+
+// using `for...of` loop
+for (const [key, value] of Object.entries(birds)) {
+  console.log(`${key}: ${value}`);
+}
+
+// owl: 🦉
+// eagle: 🦅
+// duck: 🦆
+
+/* ----------------------------------------------------------------------------- */
+
+// using array destructuring
+Object.entries(birds).forEach(([key, value]) => console.log(`${key}: ${value}`));
+
+// owl: 🦉
+// eagle: 🦅
+// duck: 
+
+/* ----------------------------------------------------------------------------- */
+
+// Converting an Object to a Map 
+const map = new Map(Object.entries(birds));
+
+console.log(map.size); // 3
+console.log(map.has('owl')); // true
+console.log(map.get('duck')); // 🦆
+
+/* ----------------------------------------------------------------------------- */
+
+const foods = {
+  cake: '🍰',
+  pizza: '🍕',
+  candy: '🍬',
+  icecream: '🍨'
+};
+
+const values = Object.values(foods);
+console.log(values);
+
+// ['🍰', '🍕', '🍬', '🍨']
+
+/* ----------------------------------------------------------------------------- */
+
+/* Both Object.values() and Object.entries() do not follow the prototype chain and 
+only iterate through the properties that are directly added to the given object. 
+They also ignore all non-enumerable properties as well: */
+
+Object.defineProperty(foods, 'sushi', {
+  value: '🍣',
+  writable: true,
+  configurable: true,
+  enumerable: false
+});
+
+console.log(Object.values(foods));
+
+// ['🍰', '🍕', '🍬', '🍨']
+
+/* ----------------------------------------------------------------------------- */
+
+// Converting an Object to a Set
+const set = new Set(Object.values(foods));
+
+console.log(set.size); // 4
+console.log(set.has('🍨')); // true
